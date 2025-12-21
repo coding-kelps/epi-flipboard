a3f9f02d3e5dfb15b3cc421a5d713020d05f2f6f
Guilhem Santé <76043368+guilhem-sante@users.noreply.github.com>
2025-12-21 01:29:48 +0100

    feat(webserver): OpenTelemetry tracing sdk

---
+++ webserver/instrumentation.node.ts
@@ -0,0 +1,19 @@
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc'
+import { resourceFromAttributes } from '@opentelemetry/resources'
+import { NodeSDK } from '@opentelemetry/sdk-node'
+import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-node'
+import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions'
+ 
+
+if (process.env.OTEL_ENABLED == '1') {
+  const sdk = new NodeSDK({
+    resource: resourceFromAttributes({
+      [ATTR_SERVICE_NAME]: 'epi-flipboard-webserver',
+    }),
+    spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter({
+      url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+    })),
+  })
+
+  sdk.start()
+}
