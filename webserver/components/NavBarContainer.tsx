6cb4c6941fc082a58e41783196d22664013c624d
Guilhem Santé <76043368+guilhem-sante@users.noreply.github.com>
2026-01-19 18:38:42 +0100

    feat(webserver): enhances topic suggestion
    Enhance navbar topic suggestion by picking top referred tags from last 24h.

--- webserver/components/NavBarContainer.tsx
+++ webserver/components/NavBarContainer.tsx
@@ -5,15 +5,40 @@ import NavBar from "./NavBar";
 export default async function NavBarContainer() {
     try {
         const prisma = getPrismaContent();
-        const tags = await prisma.tags.findMany({
-            orderBy: {
-                created_at: 'desc',
+        const topTags = await prisma.article_tag.groupBy({
+          by: ['tag_id'],
+          where: {
+            articles: {
+              published_at: {
+                gte: new Date(Date.now() - 24 * 60 * 60 * 1000),
+              },
             },
-            take: 10,
+          },
+          _count: {
+            article_id: true,
+          },
+          orderBy: {
+            _count: {
+              article_id: 'desc',
+            },
+          },
+          take: 10,
         });
-    
-        const tagNames = tags.map((tag) => tag.name);
 
+        const tagIds = topTags.map(t => t.tag_id);
+
+        const tags = await prisma.tags.findMany({
+          where: {
+            tag_id: { in: tagIds },
+          },
+          select: {
+            tag_id: true,
+            name: true,
+          },
+        });
+
+        const tagNames = topTags.map(t => tags.find(tag => tag.tag_id === t.tag_id)!.name);
+    
         return <NavBar items={tagNames} />;
     } catch (e) {
         return null;
